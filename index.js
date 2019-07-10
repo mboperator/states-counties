9c781554e3ad0ca19316f834e29d586a2765b1ed
Marcus Bernales <marcusbernales@gmail.com>
2019-07-10 10:58:10 -0700

    Modified default exports

--- index.js
+++ index.js
@@ -1,52 +1,50 @@
-export { default } from './AK'
-export { default } from './LA'
-export { default } from './OK'
-export { default } from './AL'
-export { default } from './MA'
-export { default } from './OR'
-export { default } from './AR'
-export { default } from './MD'
-export { default } from './PA'
-export { default } from './AZ'
-export { default } from './ME'
-export { default } from './Prince of Wales-Outer Ketchika'
-export { default } from './CA'
-export { default } from './MI'
-export { default } from './RI'
-export { default } from './CO'
-export { default } from './MN'
-export { default } from './SC'
-export { default } from './CT'
-export { default } from './MO'
-export { default } from './SD'
-export { default } from './DE'
-export { default } from './MS'
-export { default } from './TN'
-export { default } from './District of Columbia'
-export { default } from './MT'
-export { default } from './TX'
-export { default } from './FL'
-export { default } from './NC'
-export { default } from './UT'
-export { default } from './GA'
-export { default } from './ND'
-export { default } from './VA'
-export { default } from './HI'
-export { default } from './NE'
-export { default } from './VT'
-export { default } from './IA'
-export { default } from './NH'
-export { default } from './WA'
-export { default } from './ID'
-export { default } from './NJ'
-export { default } from './WI'
-export { default } from './IL'
-export { default } from './NM'
-export { default } from './WV'
-export { default } from './IN'
-export { default } from './NV'
-export { default } from './WY'
-export { default } from './KS'
-export { default } from './NY'
-export { default } from './KY'
-export { default } from './OH'
+export { default as AK } from "./AK";
+export { default as LA } from "./LA";
+export { default as OK } from "./OK";
+export { default as AL } from "./AL";
+export { default as MA } from "./MA";
+export { default as OR } from "./OR";
+export { default as AR } from "./AR";
+export { default as MD } from "./MD";
+export { default as PA } from "./PA";
+export { default as AZ } from "./AZ";
+export { default as ME } from "./ME";
+export { default as CA } from "./CA";
+export { default as MI } from "./MI";
+export { default as RI } from "./RI";
+export { default as CO } from "./CO";
+export { default as MN } from "./MN";
+export { default as SC } from "./SC";
+export { default as CT } from "./CT";
+export { default as MO } from "./MO";
+export { default as SD } from "./SD";
+export { default as DE } from "./DE";
+export { default as MS } from "./MS";
+export { default as TN } from "./TN";
+export { default as MT } from "./MT";
+export { default as TX } from "./TX";
+export { default as FL } from "./FL";
+export { default as NC } from "./NC";
+export { default as UT } from "./UT";
+export { default as GA } from "./GA";
+export { default as ND } from "./ND";
+export { default as VA } from "./VA";
+export { default as HI } from "./HI";
+export { default as NE } from "./NE";
+export { default as VT } from "./VT";
+export { default as IA } from "./IA";
+export { default as NH } from "./NH";
+export { default as WA } from "./WA";
+export { default as ID } from "./ID";
+export { default as NJ } from "./NJ";
+export { default as WI } from "./WI";
+export { default as IL } from "./IL";
+export { default as NM } from "./NM";
+export { default as WV } from "./WV";
+export { default as IN } from "./IN";
+export { default as NV } from "./NV";
+export { default as WY } from "./WY";
+export { default as KS } from "./KS";
+export { default as NY } from "./NY";
+export { default as KY } from "./KY";
+export { default as OH } from "./OH";
