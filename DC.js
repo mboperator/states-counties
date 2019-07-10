9cb067d8ff1922714983cfdafa43e09ae4091661
Marcus Bernales <marcusbernales@gmail.com>
2019-07-10 11:05:37 -0700

    Added name to DC

--- DC.js
+++ DC.js
@@ -1,11 +1,12 @@
 export default {
-  state: "District of Columbia",
+  state: "DC",
+  name: "District of Columbia",
   counties: [
     {
       msa: 8872,
       pmsa: 8840,
       county: "District of Columbia",
-      state: "District of Columbia",
+      state: "DC",
       county_fips: 1,
       state_fips: 11
     }
